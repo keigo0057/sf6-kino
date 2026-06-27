5a661109d8bc7efba2fcc4ea613051208a8af749
山田 太郎 <test>
2026-06-27 15:27:09 +0900

    JSON化、キャラ一覧の自動生成

--- js/app.js
+++ js/app.js
@@ -4,9 +4,74 @@ document.getElementById("hamburger");
 const sidebar =
 document.getElementById("sidebar");
 
+const characterList =
+document.getElementById("characterList");
 
-hamburger.onclick=()=>{
 
-sidebar.classList.toggle("open");
+
+hamburger.onclick = ()=>{
+
+    sidebar.classList.toggle("open");
+
+};
+
+
+
+
+async function loadCharacters(){
+
+    const response =
+    await fetch("./data/characters.json");
+
+
+    const characters =
+    await response.json();
+
+
+    renderCharacterList(
+        characters
+    );
 
 }
+
+
+
+function renderCharacterList(characters){
+
+    characterList.innerHTML="";
+
+
+    characters.forEach(character=>{
+
+
+        const div =
+        document.createElement("div");
+
+
+        div.className=
+        "characterCard";
+
+
+        div.textContent=
+        character.name;
+
+
+
+        div.dataset.id=
+        character.id;
+
+
+
+        characterList.appendChild(div);
+
+
+
+    });
+
+
+
+}
+
+
+
+loadCharacters();
