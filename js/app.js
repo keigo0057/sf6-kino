174082e86ae81a2dbd5a78a3bfd34e3a86dc9aa3
山田 太郎 <test>
2026-06-27 15:22:57 +0900

    ベース画面

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const hamburger =
+document.getElementById("hamburger");
+
+const sidebar =
+document.getElementById("sidebar");
+
+
+hamburger.onclick=()=>{
+
+sidebar.classList.toggle("open");
+
+}
